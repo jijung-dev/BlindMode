9fc0f26802c708897215e960e3b74d54459813eb
TGEStudio <tge2252@gmail.com>
2025-04-12 23:39:02 +0700

    Init Commit

---
+++ BlindMode.cs
@@ -0,0 +1,149 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Deadpan.Enums.Engine.Components.Modding;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.AddressableAssets;
+using UnityEngine.AddressableAssets.ResourceLocators;
+using System;
+using UnityEngine.Localization.Tables;
+using UnityEngine.Localization.Components;
+using System.Collections;
+
+
+namespace BlindModeMode
+{
+    public class BlindMode : WildfrostMod
+    {
+        public BlindMode(string modDir)
+            : base(modDir)
+        {
+            HarmonyInstance.PatchAll(typeof(PatchHarmony));
+        }
+        public static BlindMode instance;
+        public static List<object> assets = new List<object>();
+        public static bool preload = false;
+        public override string GUID => "tgestudio.wildfrost.blindmode";
+
+        public override string[] Depends => new string[] { };
+
+        public override string Title => "Blind Mode";
+
+        public override string Description =>
+        "Got Blinded";
+        public override List<T> AddAssets<T, Y>()
+        {
+            if (assets.OfType<T>().Any())
+                Debug.LogWarning($"[{Title}] adding {typeof(Y).Name}s: {assets.OfType<T>().Count()}");
+            return assets.OfType<T>().ToList();
+        }
+        public T TryGet<T>(string name) where T : DataFile
+        {
+            T data;
+            if (typeof(StatusEffectData).IsAssignableFrom(typeof(T)))
+                data = base.Get<StatusEffectData>(name) as T;
+            else if (typeof(KeywordData).IsAssignableFrom(typeof(T)))
+                data = base.Get<KeywordData>(name.ToLower()) as T;
+            else
+                data = base.Get<T>(name);
+
+            if (data == null)
+                throw new Exception($"TryGet Error: Could not find a [{typeof(T).Name}] with the name [{name}] or [{Extensions.PrefixGUID(name, this)}]");
+
+            return data;
+        }
+
+        public override void Load()
+        {
+            instance = this;
+            Events.OnCardDataCreated += NoSee;
+            Events.OnEntityDisplayUpdated += Add;
+            base.Load();
+        }
+
+        private void Add(Entity entity)
+        {
+            switch (entity.data.cardType.name)
+            {
+                case "Enemy":
+                case "Boss":
+                case "Miniboss":
+                case "BossSmall":
+                    Change(entity);
+                    break;
+                case "Clunker":
+                    if (entity.data.isEnemyClunker)
+                        Change(entity);
+                    break;
+            }
+        }
+        private void Change(Entity target)
+        {
+            if ((bool)target.gameObject.GetComponentInChildren<Canvas>().transform.Find("=")) return;
+            var mask = target.gameObject.GetComponentInChildren<Canvas>().transform.Find("Front");
+            GameObject back = target.gameObject.GetComponentInChildren<Canvas>().transform.Find("Back").gameObject;
+            var backClone = GameObject.Instantiate(back, target.gameObject.GetComponentInChildren<Canvas>().transform);
+            backClone.name = "=";
+            backClone.SetActive(true);
+            backClone.GetComponent<AddressableSpriteLoader>().Destroy();
+
+            foreach (var frame in mask.GetAllChildren())
+            {
+                if (frame.name.Contains("Mask"))
+                {
+                    frame.GetComponent<Image>().enabled = false;
+                    foreach (Transform item in frame)
+                    {
+                        if (item.name != "Interaction") item.gameObject.SetActive(false);
+                    }
+                    continue;
+                }
+                frame.gameObject.SetActive(false);
+            }
+        }
+        internal Sprite ScaledSprite(string fileName, int pixelsPerUnit = 100)
+        {
+            Texture2D tex = ImagePath(fileName).ToTex();
+
+            // Convert 2-pixel offset to normalized Y pivot offset
+            float offsetY = 110f / tex.height;
+
+            return Sprite.Create(
+                tex,
+                new Rect(0, 0, tex.width, tex.height),
+                new Vector2(0.5f, (20f * pixelsPerUnit) / (tex.height * 100f) + offsetY),
+                pixelsPerUnit
+            );
+        }
+
+        private void NoSee(CardData card)
+        {
+            switch (card.cardType.name)
+            {
+                case "Enemy":
+                case "Boss":
+                case "Miniboss":
+                case "BossSmall":
+                    Flip(card);
+                    break;
+                case "Clunker":
+                    if (card.isEnemyClunker)
+                        Flip(card);
+                    break;
+            }
+        }
+        void Flip(CardData card)
+        {
+            card.forceTitle = "???";
+        }
+        public CardData.StatusEffectStacks SStack(string name, int amount) => new CardData.StatusEffectStacks(TryGet<StatusEffectData>(name), amount);
+
+        public override void Unload()
+        {
+            base.Unload();
+            Events.OnCardDataCreated -= NoSee;
+            Events.OnEntityDisplayUpdated -= Add;
+        }
+    }
+}
---
+++ PatchStuffs/PatchNoHover.cs
@@ -0,0 +1,67 @@
+using HarmonyLib;
+using static Deadpan.Enums.Engine.Components.Modding.WildfrostMod;
+using BlindModeMode;
+using UnityEngine.Localization.Tables;
+using Deadpan.Enums.Engine.Components.Modding;
+using UnityEngine;
+using UnityEngine.Localization.Components;
+
+[HarmonyPatch(typeof(CardPopUpTarget), nameof(CardPopUpTarget.Pop))]
+class PatchNoHover
+{
+	static bool Prefix(CardPopUpTarget __instance)
+	{
+		if ((bool)__instance.card?.entity?.gameObject.GetComponentInChildren<Canvas>().transform.Find("="))
+			return false;
+		else
+			return true;
+	}
+}
+[HarmonyPatch(typeof(InspectSystem), nameof(InspectSystem.Inspect), typeof(Entity))]
+class PatchNoInspect
+{
+	static bool Prefix(Entity entity)
+	{
+		if ((bool)entity.gameObject.GetComponentInChildren<Canvas>().transform.Find("="))
+			return false;
+		else
+			return true;
+	}
+}
+[HarmonyPatch(typeof(MapNodeSpriteSetterBattle), nameof(MapNodeSpriteSetterBattle.Set), typeof(MapNode))]
+class PatchChangeSprite
+{
+	static bool Prefix(MapNodeSpriteSetterBattle __instance, MapNode mapNode)
+	{
+		if ((bool)__instance.@base)
+		{
+			AreaData areaData = References.Areas[mapNode.campaignNode.areaIndex];
+			__instance.@base.sprite = areaData.battleBaseSprite;
+		}
+
+		if (mapNode.campaignNode.type is CampaignNodeTypeBattle && mapNode.campaignNode.data.TryGetValue("battle", out var value) && value is string assetName)
+		{
+			BattleData battleData = AddressableLoader.Get<BattleData>("BattleData", assetName);
+			if ((object)battleData != null)
+			{
+				StringTable uiText = LocalizationHelper.GetCollection("UI Text", SystemLanguage.English);
+
+				string key = mapNode.name + "Ribbon";
+				uiText.SetString(key, "???");
+
+				__instance.icon.sprite = BlindMode.instance.ScaledSprite("Mystery.png", 200);
+				if ((bool)__instance.battleNameString)
+				{
+					__instance.battleNameString.StringReference = uiText.GetString(key);
+				}
+			}
+		}
+
+		if (mapNode.campaignNode.cleared && (bool)__instance.flagObj)
+		{
+			__instance.flagObj.SetActive(value: true);
+			__instance.iconObj.SetActive(value: false);
+		}
+		return false;
+	}
+}
